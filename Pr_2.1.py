fc58bb3f33b9b941328a18e84591bd31be650f4a
VeRsASRomAA <157576722+VeRsASRomAA@users.noreply.github.com>
2024-02-14 14:30:49 +0300

    Pr_2.1.py

--- Pr_2.1.py
+++ Pr_2.1.py
@@ -30,3 +30,18 @@ else:
     else:
         min=y
     print("3) z=", max)
+    
+#Задача 3
+x = int(input())
+y = int(input())
+z = int(input())
+minXYZ = x
+if x >=z:
+    maxXZ = x
+else:
+    maxXZ = z
+if y < minXYZ:
+    minXYZ = y
+if z < minXYZ:
+    minXYZ = z
+print("L =", 2*maxXZ - 3*minXYZ)
